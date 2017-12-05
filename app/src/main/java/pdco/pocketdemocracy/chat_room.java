7b7ad9b64f7f335a3d7cc1611325250224d0500a
kianb <kianbadie@gmail.com>
2017-12-04 19:13:44 -0800

    124

--- app/src/main/java/pdco/pocketdemocracy/chat_room.java
+++ app/src/main/java/pdco/pocketdemocracy/chat_room.java
@@ -1,5 +1,6 @@
 package pdco.pocketdemocracy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -12,15 +13,22 @@ import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by CaveMonster on 11/1/2017.
  */
 
-public class chat_room extends AppCompatActivity {
+public class chat_room extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseListAdapter<message> adapter;
+    private FloatingActionButton addVote;
+    private DatabaseReference voteReference;
+    private DatabaseReference roomReference;
 
 
     @Override
@@ -49,7 +57,37 @@ public class chat_room extends AppCompatActivity {
                 input.setText("");
             }
         });
+
+        addVote = (FloatingActionButton) findViewById(R.id.addVote);
+        addVote.setOnClickListener(this);
+
+        if(voteReference == null){
+            roomReference = FirebaseDatabase.getInstance().getReference();
+            roomReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    if (!snapshot.hasChildren()) {
+                        //Toast.makeText(chat_room.this, "Start up", Toast.LENGTH_SHORT).show();
+                        voteReference = FirebaseDatabase.getInstance().getReference().push();
+                        voteReference.setValue(new vote());
+                    }
+                    else{
+                        for(DataSnapshot child: snapshot.getChildren()){
+                            voteReference = child.getRef();
+                            break;
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Toast.makeText(chat_room.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
         displayChatMessages();
+
     }
 
 
@@ -57,7 +95,7 @@ public class chat_room extends AppCompatActivity {
         ListView listOfMessages = (ListView)findViewById(R.id.list_of_messages);
 
         adapter = new FirebaseListAdapter<message>(this, message.class,
-                R.layout.activity_message, FirebaseDatabase.getInstance().getReference()) {
+                R.layout.activity_message, FirebaseDatabase.getInstance().getReference().child("Messages")) {
             @Override
             protected void populateView(View v, message model, int position) {
                 // Get references to the views of message.xml
@@ -78,5 +116,17 @@ public class chat_room extends AppCompatActivity {
         listOfMessages.setAdapter(adapter);
     }
 
-
+    public void addVote(){
+        Bundle b = new Bundle();
+        b.putString("voteReference", voteReference.toString());
+        Intent intent = new Intent(chat_room.this, create_vote.class);
+        intent.putExtras(b);
+        startActivity(intent);
+    }
+    @Override
+    public void onClick(View view) {
+        if(view == addVote){
+            addVote();
+        }
+    }
 }
---
+++ app/src/main/java/pdco/pocketdemocracy/create_vote.java
@@ -0,0 +1,46 @@
+package pdco.pocketdemocracy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class create_vote extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText voteTitle;
+    private Button castVote;
+    private DatabaseReference voteReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_vote);
+
+        voteTitle = (EditText) findViewById(R.id.voteTitle);
+        castVote = (Button) findViewById(R.id.castVote);
+
+        castVote.setOnClickListener(this);
+
+        Bundle b = getIntent().getExtras();
+        String reference = b.getString("voteReference");
+        voteReference = FirebaseDatabase.getInstance().getReferenceFromUrl(reference);
+
+    }
+
+    public void sendVote(){
+        String voteTitleText = voteTitle.getText().toString();
+        voteReference.setValue(new vote(voteTitleText, ""));
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view == castVote){
+            sendVote();
+        }
+
+    }
+}
